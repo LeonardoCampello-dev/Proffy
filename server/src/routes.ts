541b68697549c864b64942b044f30454a72d2f42
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-08-05 14:53:19 -0300

    Create routes and controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,56 +1,12 @@
 import express from 'express'
-import db from './database/connection'
-
-import convertHourToMinutes from './utils/convertHourToMinutes'
+import ClassesController from './controllers/ClassesController'
 
 const routes = express.Router()
+const classesControllers = new ClassesController()
 
-interface ScheduleItem {
-    week_day: number,
-    from: string,
-    to: string,
-}
 
-routes.post('/classes', async (req, res) => {
-    const { 
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = req.body
+routes.get('/classes', classesControllers.index)
+routes.post('/classes', classesControllers.create)
 
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    })
-
-    const user_id = insertedUsersIds[0]
-
-    const insertedClassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id,
-    })
-
-    const class_id = insertedClassesIds[0]
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to),
-        }
-    })
-
-    await db('class_schedule').insert(classSchedule)
-    
-    return res.send()
-})
 
 export default routes
