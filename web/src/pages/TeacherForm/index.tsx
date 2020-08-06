8d32c8d4f06c29bd69c41b04d0252026cdf59d43
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-08-05 21:26:06 -0300

    Components for select and textarea created

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
 import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
@@ -22,12 +24,27 @@ function TeacherForm() {
                     <Input name="name" label="Nome completo"/>
                     <Input name="avatar" label="Avatar"/>
                     <Input name="whatsapp" label="WhatsApp"/>
+                    <Textarea name="bio" label="Biografia"/>
                 </fieldset>
 
                 <fieldset>
                     <legend>Sobre a aula</legend>
                     
-                    <Input name="subject" label="Matéria"/>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciência', label: 'Ciência' },
+                            { value: 'Educação Física', label: 'Educação Física' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Química', label: 'Química' },
+                        ]}
+                    />
                     <Input name="cost" label="Custo da sua hora por aula"/>
                 </fieldset>
 
