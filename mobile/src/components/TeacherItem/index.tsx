60b0997a6f58254f5f11a375f14e7894e2959543
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-08-08 21:46:09 -0300

    Connected API / Teacher List / Total Connections

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -8,36 +8,46 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number
+    avatar: string
+    bio: string
+    cost: number
+    name: string
+    subject: string
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image
                     style={styles.avatar}
-                    source={{ uri: 'https://github.com/LeonardoCampello-dev.png' }}
+                    source={{ uri: teacher.avatar }}
                 />
 
                 <View style={styles.profileInfo}>
                     <Text style={styles.name}>
-                        Leonardo Campello
+                    { teacher.name }
                     </Text>
 
                     <Text style={styles.subject}>
-                    Educação Física
+                    { teacher.subject }
                     </Text>
                 </View>
             </View>
 
-            <Text style={styles.bio}>
-                Instrutor de Educação Física para iniciantes, minha missão de vida é levarsaúde e contribuir para o crescimento de quem se interessar.
-                {'\n'}{'\n'}
-                Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: "Aprenda a fazer dinheiro com isso!"
-            </Text>
+            <Text style={styles.bio}>{ teacher.bio }</Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/Hora {'   '}
-                    <Text style={styles.priceValue}>R$ 20,00 </Text>
+                    <Text style={styles.priceValue}>R$ { teacher.cost } </Text>
                 </Text>
 
                 <View style={styles.buttonsContainer}>
