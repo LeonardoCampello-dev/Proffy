9a1486f63231d5a0517768feb81f4d0b1271ef33
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-08-08 23:00:13 -0300

    Adding and removing favorites

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { View, ScrollView, Text, TextInput } from 'react-native'
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem, { Teacher } from '../../components/TeacherItem'
@@ -13,18 +14,34 @@ import styles from './styles'
 function TeacherList() {
     const [isFiltersVisible, setIsFilterVisible] = useState(false)
 
-    const[teachers, setTeachers] = useState([])
-    
+    const [teachers, setTeachers] = useState([])
+    const [favorites, setFavorites] = useState<number[]>([])
+
     const [subject, setSubject] = useState('')
     const [week_day, setWeekDay] = useState('')
     const [time, setTime] = useState('')
 
-    
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response)
+                const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+                    return teacher.id
+                })
+
+                setFavorites(favoritedTeachersIds)
+            }
+        })
+    }
+
     function handleToggleFiltersVisible() {
         setIsFilterVisible(!isFiltersVisible)
     }
 
     async function handleFiltersSubmit() {
+        loadFavorites()
+        
         const response = await api.get('classes', {
             params: {
                 subject,
@@ -36,18 +53,18 @@ function TeacherList() {
         setIsFilterVisible(false)
         setTeachers(response.data)
     }
-    
+
     return (
         <View style={styles.container}>
-            <PageHeader 
-                title="Proffys disponíveis" 
+            <PageHeader
+                title="Proffys disponíveis"
                 headerRight={(
                     <BorderlessButton onPress={handleToggleFiltersVisible}>
-                        <Feather name="filter" size={20} color="#FFF" />        
+                        <Feather name="filter" size={20} color="#FFF" />
                     </BorderlessButton>
                 )}
             >
-                { isFiltersVisible && (
+                {isFiltersVisible && (
                     <View style={styles.searchForm}>
                         <Text style={styles.label}>Matéria</Text>
                         <TextInput
@@ -97,9 +114,15 @@ function TeacherList() {
                     paddingHorizontal: 16,
                     paddingBottom: 16
                 }}
-            >   
+            >
                 {teachers.map((teacher: Teacher) => {
-                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                    return (
+                        <TeacherItem
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited={favorites.includes(teacher.id)}
+                        />
+                    )
                 })}
             </ScrollView>
         </View>
